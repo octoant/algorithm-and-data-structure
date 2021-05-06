2df04f1265dbb3bfdaf97e9a745d9e19aeaf43b9
Bobur Zakirov <bobur.octo@gmail.com>
2021-05-07 00:22:21 +0300

    Hope last commit

--- timus/4.graph-algorithms/mobile-telegraphs [optimized].cpp
+++ timus/4.graph-algorithms/mobile-telegraphs [optimized].cpp
@@ -1,48 +1,73 @@
 //
-// Created by Bobur on 04.05.2021
+// Created by Bobur on 06.05.2021
 //
-#include <stdio.h>
-#include <algorithm>
+#include <bits/stdc++.h>
 
-#define parent(i) (i - 1) >> 1
-#define left(i) (i << 1) + 1
-#define right(i) (i << 1) + 2
+# define parent(i) (i - 1) >> 1
+# define left(i) (i << 1) + 1
+# define right(i) (i << 1) + 2
 
 using namespace std;
 
-const int infinity = 1 << 30;
-
-const int maxn = 50000;
-const int w = 135;
-const int digit = 10;
-
-long long p[digit];
+long long p[10]; /* ..., 1000, 100, 10, 1 */
 int N;
-int cost[digit];
-pair<long long, int> number[maxn];
-
-int idx[maxn], adj[maxn][w];
-
-int len = 0, heap[maxn];
-int weight[maxn], indices[maxn];
-bool visited[maxn], in[maxn];
+int cost[10];
+struct node { long long val; int idx; } number[50000];
+int len = 0;
+int heap[50000], weight[50000], idx[50000];
+bool visited[50000], in[50000];
 
 int get_idx(long long elem)
 {
     int left = 0, mid, right = N-1;
     while (left <= right) {
         mid = (left + right) >> 1;
-        if (number[mid].first < elem)
+        if (number[mid].val < elem)
             left = mid + 1;
         else
-            if (number[mid].first > elem)
-                right = mid - 1;
-            else
-                return mid;
+        if (number[mid].val > elem)
+            right = mid - 1;
+        else
+            return mid;
     }
     return -1;
 }
 
+void sift_up(int i)
+{
+    while (i && weight[heap[i]] < weight[heap[parent(i)]]) {
+        swap(heap[i], heap[parent(i)]);
+        i = parent(i);
+    }
+}
+
+void sift_down(int i)
+{
+    while (true) {
+        int j = i;
+        if (left(j) < len && weight[heap[i]] > weight[heap[left(j)]]) {
+            i = left(j);
+        }
+        if (right(j) < len && weight[heap[i]] > weight[heap[right(j)]]) {
+            i = right(j);
+        }
+        if (i != j) swap(heap[i], heap[j]);
+        else break;
+    }
+}
+
+void add(long long elem)
+{
+    heap[len] = elem;
+    sift_up(len++);
+}
+
+int get_min()
+{
+    swap(heap[0], heap[--len]);
+    sift_down(0);
+    return heap[len];
+}
 
 int get_digit(long long elem, int i)
 {
@@ -56,152 +81,80 @@ long long set_digit(long long elem, int i, int j)
 
 long long swap_digits(long long elem, int i, int j)
 {
-    int ti = get_digit(elem, i);
-    int tj = get_digit(elem, j);
-    return set_digit(set_digit(elem, i, tj), j, ti);
+    int dig_i = get_digit(elem, i);
+    int dig_j = get_digit(elem, j);
+    return set_digit(set_digit(elem, i, dig_j), j, dig_i);
 }
 
-
-int get_cost(int i, int j)
-{
-    for (int k = 0; k < digit; k++) {
-        if (get_digit(number[i].first, k) != get_digit(number[j].first, k))
-            return cost[k];
-    }
-    return -1;
-}
-
-
-void add(long long elem, int id)
+void relax(long long elem, int id, int x)
 {
     int i = get_idx(elem);
     int j = id;
-    if (i != -1 && number[i].second < number[j].second) {
-        adj[i][idx[i]++] = j;
-        adj[j][idx[j]++] = i;
-    }
-}
-
-void add_next(int id)
-{
-    for (int i = 0; i < digit; i++) {
-        for (int j = 0; j < digit; j++) {
-            add(set_digit(number[id].first, i, j), id);
+    if (i != -1 && i != j)
+        if (!visited[i] && weight[i] > weight[j] + cost[x]) {
+            weight[i] = weight[j] + cost[x];
+            idx[i] = j;
+            if (!in[i]) in[i] = true, add(i);
         }
-        for (int j = i+1; j < digit; j++) {
-            add(swap_digits(number[id].first, i, j), id);
-        }
-    }
 }
-
-
-bool lower(int i, int j)
-{
-    return weight[heap[i]] < weight[heap[j]];
-}
-
-void sift_up(int i)
-{
-    while (i && lower(i, parent(i))) {
-        swap(heap[i], heap[parent(i)]);
-        i = parent(i);
-    }
-}
-
-void sift_down(int i)
-{
-    while (true) {
-        int j = i;
-        if (left(j) < len && lower(left(j), i)) {
-            i = left(j);
-        }
-        if (right(j) < len && lower(right(j), i)) {
-            i = right(j);
-        }
-        if (i != j) swap(heap[i], heap[j]);
-        else break;
-    }
-}
-
-void heap_add(int item)
-{
-    heap[len] = item;
-    sift_up(len++);
-}
-
-int get_min()
-{
-    swap(heap[0], heap[--len]);
-    sift_down(0);
-    return heap[len];
-}
-
 
 void dijkstra(int start)
 {
     for (int i = 0; i < N; i++) {
-        weight[i] = infinity;
+        weight[i] = 1e9;
     }
     weight[start] = 0;
-
-    heap_add(start);
+    add(start);
     while (len > 0) {
-        int i = get_min();
-        visited[i] = true;
-        for (int k = 0; k < idx[i]; k++) {
-            int j = adj[i][k];
-            int cost = get_cost(i, j);
-            if (!visited[j] && weight[j] > weight[i] + cost) {
-                weight[j] = weight[i] + cost;
-                indices[j] = i;
-                if (!in[j])
-                    in[j] = true, heap_add(j);
+        int id = get_min(); visited[id] = true;
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10; j++) {
+                relax(set_digit(number[id].val, i, j), id, i);
+            }
+            for (int j = i+1; j < 10; j++) {
+                relax(swap_digits(number[id].val, i, j), id, i);
             }
         }
     }
 }
 
-
 int main()
 {
-    long long a, b, oo = 1;
-    for (int i = 0; i < digit; i++) {
-        p[digit-i-1] = oo, oo *= 10;
-    }
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
 
-    scanf("%d", &N);
-    for (int i = 0; i < digit; i++) {
-        scanf("%d", &cost[i]);
+    long long a, b, x = 1;
+    for (int i = 0; i < 10; i++) {
+        p[9-i] = x, x *= 10;
+    }
+    cin >> N;
+    for (int i = 0; i < 10; i++) {
+        cin >> cost[i];
     }
     for (int i = 0; i < N; i++) {
-        scanf("%lld", &oo);
-        number[i] = {oo, i};
+        cin >> x;
+        number[i] = {x, i};
     }
-
-    a = number[0].first;
-    b = number[N-1].first;
-
-    sort(number, number + N);
-    for (int i = 0; i < N; i++) {
-        add_next(i);
-    }
-    int x = get_idx(a), y = get_idx(b);
-
-    dijkstra(x);
-    if (!visited[y])
-        return printf("%d\n", -1), 0;
+    a = number[0].val, b = number[N-1].val;
+    sort(number, number + N, [](node &a, node &b) {
+        return a.val < b.val;
+    });
+    int p = get_idx(a), q = get_idx(b);
+    dijkstra(p);
+    if (visited[q])
+        cout << weight[q] << endl;
     else
-        printf("%d\n", weight[y]);
+        return cout << "-1\n", 0;
 
-    int l = 0;
-    for (int i = y; i != x; i = indices[i]) {
-        idx[l++] = i;
+    len = 0;
+    for (int i = q; i != p; i = idx[i]) {
+        heap[len++] = i;
     }
-    idx[l++] = x;
-    printf("%d\n", l);
-    len = l;
-    for (int i = 0; i < len; i++) {
-        printf("%d ", number[idx[--l]].second + 1);
+    heap[len++] = p;
+    cout << len << endl;
+    for (int i = len-1; i >= 0; i--) {
+        cout << number[heap[i]].idx + 1 << " ";
     }
     return 0;
 }
